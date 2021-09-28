d8b89e2c90d0eef8f9db22a41b9781088b546a8a
Kyle Howard <kyle.howard@smsja.net>
2021-09-27 22:00:25 -0500

    adds auth management definitions

---
+++ authman.go
@@ -0,0 +1,46 @@
+package uman
+
+import "net/http"
+
+type MiddlewareFilter interface {
+	// takes HandlerFunc h, returns a function which verifies user's access
+	// and calls the passed function hon success
+	Filter(h http.HandlerFunc) http.HandlerFunc
+}
+
+type Authenticator interface {
+	//accepts user details and given password, compares equality and returns auth type
+	Authenticate(u Credentials, password string) (Authentication, error)
+}
+type AuthManager interface {
+	// //accepts username and password and transforms to custom user details type
+	// NewCredentials(sub interface{}, password string) Credentials
+	Authenticator
+	MiddlewareFilter
+}
+
+type Authentication interface {
+	//retrieves string details about authentication jwt, session etc
+	Auth() string
+}
+
+//interface to represent user credentials
+type Credentials interface {
+	Password
+	Identifier
+}
+
+type Identifier interface {
+	//retrieves the principal associated with the credentials eg. username, id etc
+	Identity() string
+}
+
+type Hasher interface {
+	//returns a string representation of the hash of the stored password
+	Hash() (string, error)
+}
+type Password interface {
+	//returns the given password
+	Password() string
+	Hasher
+}
